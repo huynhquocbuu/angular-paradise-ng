f9e5d47a79baf50022283923ca956748c904842a
cagatay.civici <cagatay.civici@fd9223c1-c77d-41e1-af93-17165896985c>
2017-05-24 08:01:13 +0000

    docs

--- src/app/demo/view/documentation.ts
+++ src/app/demo/view/documentation.ts
@@ -5,26 +5,12 @@ import {Component} from '@angular/core';
     styles: [`
         .docs pre {
             font-family: monospace;
-            background-color: #545b61;
-            color: #ffffff;
+            background-color: #dee4e9;
+            color: ##757575;
             padding: 1em;
             font-size: 14px;
             border-radius: 3px;
             overflow: auto;
-        }
-        
-        .video-container {
-            position: relative;
-            width: 100%;
-            height: 0;
-            padding-bottom: 56.25%;
-        }
-        .video-container iframe {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
         }`
     ]
 })
