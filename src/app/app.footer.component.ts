7ae5755f9855e9b119103b2f03157fe79c39c4d9
sincan.mert <sincan.mert@fd9223c1-c77d-41e1-af93-17165896985c>
2017-08-18 08:20:25 +0000

    Added footer component

--- src/app/app.footer.component.ts
+++ src/app/app.footer.component.ts
@@ -2,7 +2,16 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-footer',
-    template: ``
+    template: `
+        <div class="layout-footer clearfix">
+            <img src="assets/layout/images/logo-gray.png">
+            <div class="layout-footer-right">
+                <a href="https://github.com/primefaces"><i class="fa fa-github"></i></a>
+                <a href="https://www.facebook.com/groups/primefaces"><i class="fa fa-facebook"></i></a>
+                <a href="http://twitter.com/primefaces"><i class="fa fa-twitter"></i></a>
+            </div>
+        </div>
+    `
 })
 export class AppFooter {
 
