7b07317db31632cfc45c2d09a21ef92c18800a0c
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2021-02-09 16:10:12 +0300

    Table updated

--- src/app/demo/view/tabledemo.component.ts
+++ src/app/demo/view/tabledemo.component.ts
@@ -37,12 +37,18 @@ export class TableDemoComponent implements OnInit {
 
     rowGroupMetadata: any;
 
+    activityValues: number[] = [0, 100];
+
     @ViewChild('dt') table: Table;
 
     constructor(private customerService: CustomerService, private productService: ProductService) {}
 
     ngOnInit() {
-        this.customerService.getCustomersMedium().then(customers => this.customers1 = customers);
+        this.customerService.getCustomersLarge().then(customers => {
+            this.customers1 = customers;
+            // @ts-ignore
+            this.customers1.forEach(customer => customer.date = new Date(customer.date));
+        });
         this.customerService.getCustomersMedium().then(customers => this.customers2 = customers);
         this.customerService.getCustomersMedium().then(customers => this.customers3 = customers);
         this.productService.getProductsWithOrdersSmall().then(data => this.products = data);
